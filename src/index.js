1ba6156271911c4845dcfc3980027751d3682edf
Jake Garland <jake@richmondapps.com>
2025-05-14 08:58:14 -0700

    Reduced code coverage by adding untested code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ const { addItem, getItem } = require("./services");
 const { formatDate } = require("./utils");
 
 /**
+ * New untested function added to decrease coverage
+ */
+function newFunction() {
+  const x = 10;
+  const y = 20;
+  return x + y; // This function is intentionally left untested
+}
+
+/**
  * Main function to test the app functionality
  */
 function main() {
@@ -13,6 +22,10 @@ function main() {
   console.log("Fetched Item:", fetchedItem);
 
   console.log("Formatted Date:", formatDate(new Date()));
+
+  // Call the new untested function
+  const result = newFunction();
+  console.log("Result from newFunction:", result);
 }
 
 main();
